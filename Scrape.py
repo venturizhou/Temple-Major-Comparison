4293d47fb1c0edbe9fe294535f9178d401cbb149
Steven Zhou <venturizhou@gmail.com>
2019-11-16 16:56:19 -0500

    figured out how to get list just class names

--- Scrape.py
+++ Scrape.py
@@ -5,9 +5,28 @@ import csv
 
 response = requests.get("https://bulletin.temple.edu/undergraduate/science-technology/computer-information-science/data-science-computational-analytics-bs/#requirementstext")
 soup = BeautifulSoup(response.text, "html.parser")
-lst = []
-for title in soup.find_all("tr"):
-    lst.append((title.get_text()))
+# lst = []
+# for title in soup.find_all("tr"):
+#     lst.append((title.get_text()))
 
-for string in lst:
-    string = unicodedata.normalize(NFKD,)
+# lst = [unicodedata.normalize("NFKD",string) for string in lst]
+# for item in lst:
+#     if "Total" in item:
+#         lst = lst[:lst.index(item)]
+#         break
+    
+# print(lst)
+# lst = []
+# for title in soup.find_all("a"):
+#     lst.append(title.get_text())
+
+# lst = [unicodedata.normalize("NFKD",string) for string in lst]
+
+# def 
+lst = []
+courselist = soup.find(class_="sc_courselist")
+title = courselist.find_all("a")
+for item in title:
+    lst.append(item.get_text())
+lst = [unicodedata.normalize("NFKD",string) for string in lst]
+print(lst)
